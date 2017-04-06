c14cf69a854d6b9eb1323b3e393a39abe3fd60fa
Luis Llave <llaveluis@gmail.com>
2017-04-05 22:05:05 -0400

    [ADD] Add view to list existing posts

--- source/crud/views.py
+++ source/crud/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from django.shortcuts import render_to_response
+from django.contrib import messages
 
-# Create your views here.
+from .models import Post
+
+
+def posts(request):
+    """View to list existing posts"""
+    return render_to_response("posts.html",
+                              {"posts": Post.objects.all(),
+                               "messages": messages.get_messages(request)}
+                              )
+
+
+def add_post(request):
+    return None
+
+
+def delete_post(request):
+    return None
+
+
+def update_post(request):
+    return None
