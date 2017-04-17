147613054c5908ce9ee1b4ad4d2af812fb4a81ef
Luis Llave <llaveluis@gmail.com>
2017-04-17 17:02:56 -0400

    [FIX] Correct wrong definitions related to static files
    Static files were not visualizated, due to wrong definitions in
    settings.py.

--- source/notes/settings.py
+++ source/notes/settings.py
@@ -84,8 +84,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'notes/static'),
 )
