2d8a7389ad12a39a60bd199132c39b779dcfaed0
Luis Llave <llaveluis@gmail.com>
2017-04-18 14:27:34 -0400

    [ADD] Add urls for crud application
    Change in main settings urls. Now, each application has a urls.py file,
    and main urls.py reference to these fil these files.
    
    The main url reference to /posts/list, as desired side effect.

---
+++ source/crud/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^$', 'crud.views.posts', name='list_posts'),
+    url(r'^posts/list', 'crud.views.posts', name='list_posts'),
+    url(r'^posts/add/', 'crud.views.add_post', name='add_post'),
+    url(r'^posts/(?P<postid>\d+)/delete/', 'crud.views.delete_post',
+        name='delete_post'),
+    url(r'^posts/(?P<postid>\d+)/', 'crud.views.update_post', name='update_post'),
+]
--- source/notes/urls.py
+++ source/notes/urls.py
@@ -3,9 +3,6 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^posts/list', 'crud.views.posts', name='list_posts'),
-    url(r'^posts/add/', 'crud.views.add_post', name='add_post'),
-    url(r'^posts/(?P<postid>\d+)/delete/', 'crud.views.delete_post',
-        name='delete_post'),
-    url(r'^posts/(?P<postid>\d+)/', 'crud.views.update_post', name='update_post'),
+    url(r'posts', include('crud.urls')),
+    url(r'', include('crud.urls')),
 ]
