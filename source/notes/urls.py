02ca5dfad80e16efa50a107819c0ec0b443c07d8
Luis Llave <llaveluis@gmail.com>
2017-04-17 21:30:49 -0400

    [FIX] Change urls definition to correct delete post functionality
    Prior this change, the url for delete don't call the correct view,
    instead update view is called. This is due to similarity between url for
    delete and update.

--- source/notes/urls.py
+++ source/notes/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^posts/list', 'crud.views.posts', name='list_posts'),
     url(r'^posts/add/', 'crud.views.add_post', name='add_post'),
-    url(r'^posts/(?P<postid>\d+)/', 'crud.views.update_post', name='update_post'),
     url(r'^posts/(?P<postid>\d+)/delete/', 'crud.views.delete_post',
         name='delete_post'),
+    url(r'^posts/(?P<postid>\d+)/', 'crud.views.update_post', name='update_post'),
 ]
