85f76b6d8e8985a0847b68f86fb85a19d5d2504e
Luis Llave <llaveluis@gmail.com>
2017-04-24 12:27:30 -0400

    [IMP] Update Post model, adding support for author of post

--- source/crud/models.py
+++ source/crud/models.py
@@ -2,9 +2,12 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
 
+from entry.models import User
+
 
 class Post(models.Model):
     """Represents a redacted post"""
     title = models.CharField(max_length=80)
     content = models.TextField()
     date_pub = models.DateTimeField()
+    author = models.ForeignKey(User)
